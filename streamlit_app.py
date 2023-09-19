0f8f2a5cc78ff201cbff2447fa732b8536bda076
Abhijeet Nawale <abhijeetnawale13@gmail.com>
2023-09-19 12:07:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('Web-Data Application')
+streamlit.title('Web-Data Application for Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Masala Dosa')
 streamlit.text('Puri Bhaji')
