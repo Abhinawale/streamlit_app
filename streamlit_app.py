f150c5abeda16770116fc5893d813740c18aa53e
Abhijeet Nawale <abhijeetnawale13@gmail.com>
2023-09-19 12:06:05 +0530

    Update streamlit_app.py
    Added Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('Web-Data Application')
+streamlit.header('Brrakfast Menu')
+streamlit.text('Masala Dosa')
+streamlit.text('Puri Bhaji')
+streamlit.text('Misal-Pao')
